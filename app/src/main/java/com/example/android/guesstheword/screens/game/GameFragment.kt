4d2b64f0bd2aba0a113f76cd59b9fa0c5264494d
Thalisson <thalissonmt@gmail.com>
2021-10-10 16:15:24 -0300

    added vibrator to the game

--- app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -16,7 +16,11 @@
 
 package com.example.android.guesstheword.screens.game
 
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import android.text.format.DateUtils
 import android.util.Log
 import android.view.LayoutInflater
@@ -60,6 +64,10 @@ class GameFragment : Fragment() {
                 viewModel.onGameFinishComplete()
             }
         })
+
+        viewModel.vibrator.observe(viewLifecycleOwner, { pattern ->
+            buzz(pattern)
+        })
         return binding.root
 
     }
@@ -71,5 +79,19 @@ class GameFragment : Fragment() {
         val action = GameFragmentDirections.actionGameToScore(viewModel.score.value ?: 0)
         findNavController(this).navigate(action)
     }
+
+    private fun buzz(pattern: LongArray) {
+        val buzzer: Vibrator = activity?.getSystemService(Context.VIBRATOR_SERVICE) as Vibrator
+
+        buzzer.let {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -7,12 +7,24 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+private val NO_BUZZ_PATTERN = longArrayOf(0)
+
+enum class BuzzType(val pattern: LongArray) {
+    CORRECT(CORRECT_BUZZ_PATTERN),
+    GAME_OVER(GAME_OVER_BUZZ_PATTERN),
+    COUNTDOWN_PANIC(PANIC_BUZZ_PATTERN),
+    NO_BUZZ(NO_BUZZ_PATTERN)
+}
+
 class GameViewModel : ViewModel(){
 
     companion object {
         private const val DONE = 0L
         private const val ONE_SECOND = 1000L
-        private const val COUNTDOWN_TIME = 10000L
+        private const val COUNTDOWN_TIME = 60000L
     }
 
     private val timer: CountDownTimer
@@ -36,6 +48,10 @@ class GameViewModel : ViewModel(){
     val timerText: LiveData<String>
         get() = _timerText
 
+    private var _vibrator = MutableLiveData<LongArray>()
+    val vibrator: LiveData<LongArray>
+        get() = _vibrator
+
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
 
@@ -52,11 +68,15 @@ class GameViewModel : ViewModel(){
             override fun onTick(millisUntilFinished: Long) {
                 timerCount = timerCount.minus(ONE_SECOND)
                 _timerText.value = DateUtils.formatElapsedTime(timerCount.div(ONE_SECOND))
+                if (timerCount <= ONE_SECOND.times(5)){
+                    _vibrator.value = BuzzType.COUNTDOWN_PANIC.pattern
+                }
                 Log.i("GameViewModel", "onTick called!")
             }
 
             override fun onFinish() {
                 _eventGameFinish.value = true
+                _vibrator.value = BuzzType.GAME_OVER.pattern
             }
         }
         timer.start()
@@ -121,6 +141,7 @@ class GameViewModel : ViewModel(){
     }
 
     fun onCorrect() {
+        _vibrator.value = BuzzType.CORRECT.pattern
         _score.value = _score.value?.plus(1)
         nextWord()
     }
